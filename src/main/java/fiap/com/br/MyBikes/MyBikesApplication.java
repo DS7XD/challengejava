3789b62fbdfdab460f0861997ea5be298b3dd110
DS7XD <lucasmpp7@gmail.com>
2025-05-24 14:30:37 -0300

    a

---
+++ src/main/java/fiap/com/br/MyBikes/MyBikesApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.MyBikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBikesApplication.class, args);
+	}
+
+}
