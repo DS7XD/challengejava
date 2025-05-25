67a28110a898c82e7565e5a01616406b9fea65d1
g.limazz <167983773+limaaz1@users.noreply.github.com>
2025-05-25 12:34:57 -0300

    Update LocacaoController.java

--- src/main/java/fiap/com/br/MyBikes/Controller/LocacaoController.java
+++ src/main/java/fiap/com/br/MyBikes/Controller/LocacaoController.java
@@ -7,11 +7,11 @@ public class LocacaoController {
     public static void main(String[] args) {
         LocacaoDAO locacaoDAO = new LocacaoDAO();
 
-        // Cadastrar nova locação
+        
         Locacao nova = new Locacao(1, 1, Date.valueOf("2025-05-10"), Date.valueOf("2025-05-17"));
         locacaoDAO.cadastrarLocacao(nova);
 
-        // Listar locações
+       
         System.out.println("Lista de locações:");
         for (Locacao l : locacaoDAO.listarLocacoes()) {
             System.out.println(l);
