bc3d9445f9de1b6a5b70d04a42c1a3ccddd6052e
g.limazz <167983773+limaaz1@users.noreply.github.com>
2025-05-25 12:31:21 -0300

    Update and rename src/main/java/fiap/com/br/MyBikes/Service/UsuarioService to src/main/java/fiap/com/br/MyBikes/LocacaoService.java

---
+++ src/main/java/fiap/com/br/MyBikes/LocacaoService.java
@@ -0,0 +1,34 @@
+// Arquivo: service/LocacaoService.java
+package com.mybikes.locacoes.service;
+
+import com.mybikes.locacoes.model.Locacao;
+import com.mybikes.locacoes.repository.LocacaoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LocacaoService {
+
+    private final LocacaoRepository repository;
+
+    public LocacaoService(LocacaoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Locacao> listarTodas() {
+        return repository.findAll();
+    }
+
+    public Locacao salvar(Locacao locacao) {
+        return repository.save(locacao);
+    }
+
+    public Locacao buscarPorId(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletarPorId(Integer id) {
+        repository.deleteById(id);
+    }
+} 
