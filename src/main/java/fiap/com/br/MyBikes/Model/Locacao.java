a46b2966983f189dc9364506b9b9c5f119487dc4
g.limazz <167983773+limaaz1@users.noreply.github.com>
2025-05-24 21:46:04 -0300

    Update Locacao.java

--- src/main/java/fiap/com/br/MyBikes/Model/Locacao.java
+++ src/main/java/fiap/com/br/MyBikes/Model/Locacao.java
@@ -3,23 +3,53 @@ package model;
 import java.sql.Date;
 
 public class Locacao {
-    private int id;
+    private int idLocacao;
     private int idMoto;
-    private int idUsuario;
     private Date dataInicio;
     private Date dataFim;
 
-    public Locacao(int id, int idMoto, int idUsuario, Date dataInicio, Date dataFim) {
-        this.id = id;
+    public Locacao(int idLocacao, int idMoto, Date dataInicio, Date dataFim) {
+        this.idLocacao = idLocacao;
         this.idMoto = idMoto;
-        this.idUsuario = idUsuario;
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
     }
 
-    public int getId() { return id; }
-    public int getIdMoto() { return idMoto; }
-    public int getIdUsuario() { return idUsuario; }
-    public Date getDataInicio() { return dataInicio; }
-    public Date getDataFim() { return dataFim; }
+    public int getIdLocacao() {
+        return idLocacao;
+    }
+
+    public int getIdMoto() {
+        return idMoto;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setIdLocacao(int idLocacao) {
+        this.idLocacao = idLocacao;
+    }
+
+    public void setIdMoto(int idMoto) {
+        this.idMoto = idMoto;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    @Override
+    public String toString() {
+        return "Locacao [ID: " + idLocacao + ", Moto ID: " + idMoto +
+               ", Início: " + dataInicio + ", Fim: " + dataFim + "]";
+    }
 }
